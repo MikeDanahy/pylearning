2d82c7a50989adc9af14ec7e5c526c52c0e1ccc4
MikeDanahy <96398698+MikeDanahy@users.noreply.github.com>
2023-05-22 05:17:55 -0600

    Varialbe tutorial added

---
+++ basics/variables.py
@@ -0,0 +1,20 @@
+x = 5
+y = "John"
+print(x)
+print(y,"\n")
+
+x = 4       # x is of type int
+x = "Sally" # x is now of type str
+print(x,"\n")
+
+x = str(3)    # x will be '3'
+y = int(3)    # y will be 3
+z = float(3)  # z will be 3.0
+print(x)
+print(y)
+print(z,"\n\n")
+x = 5
+y = "John"
+print(type(x))
+print(type(y,"\n\n"))
+
