00f7ed245c16c4e74269a45e5e5bca2f4c0d7996
MikeDanahy <96398698+MikeDanahy@users.noreply.github.com>
2023-06-05 05:15:58 -0600

    Added lessons on strings

--- basics/variables.py
+++ basics/variables.py
@@ -31,3 +31,31 @@ print(x)
 print(y)
 print(z)
 
+""" Python Stings
+"""
+print("Hello World is assigned to a varialbe")
+
+
+a = " Hello, World! "
+print('Length of \'Hello World\' is ' ,len(a),"\n\n")
+print("It is easy to automatically pull characters from a string such as 'Hello, World'")
+for x in a:
+    print(x)
+    
+print("\n")
+
+print("You can easily check to see if there is text in a string with 'text in string'\n")
+print("It implies an if\n")
+print("It is", "Hello" in a," that Hello is in a")
+print("It is", "hello" in a," that hello is in a\n")
+
+print("Slicking Strings")
+print("The syntax is [x:y] which means get the of the text starting at x up to but not including y")
+print("a[2:5] of Hello, World")
+print(a[2:5],"\n\n")
+print("Get characters from beggining up to 5 with a[:5]")
+print(a[:5],"\n")
+print("You can also make a string upper/lower with <var>.lower etc")
+print(a.lower()," ",a.upper(),"\n")
+print("If you want to remove white spaces from begin or end use <var>(strip())")
+print("A with white spaces:",a,":A without white spaces:"+ a.strip()+":\n")
